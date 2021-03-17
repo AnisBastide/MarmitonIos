a3c99ca6360a858018a631abd6c4c60d2bc77737
Rosstail <rosstail_zegeek@hotmail.fr>
2021-03-17 18:37:57 +0100

    Converted to Collection

---
+++ marmitonIOS/viewControllers/RecipeCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  RecipeCollectionViewCell.swift
+//  marmitonIOS
+//
+//  Created by Anthony BAC on 17/03/2021.
+//  Copyright © 2021 Group2. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var label : UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
